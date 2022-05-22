5e01346b624b48be1906e4295d5a5c38e0ef5534
vnjaradi <valentina.njaradi@gmail.com>
2022-05-22 12:12:45 +0100

    basic parser skeleton + added new branch

--- src/parse.py
+++ src/parse.py
@@ -9,6 +9,7 @@ Classes
 Parser - parses the definition file and builds the logic network.
 """
 
+from scanner import Symbol
 
 class Parser:
     """Parse the definition file and build the logic network.
@@ -40,10 +41,50 @@ class Parser:
 
     def __init__(self, names, devices, network, monitors, scanner):
         """Initialise constants."""
+        self.names = names
+        self.devices = devices
+        self.network = network
+        self.monitors = monitors
+        self.scanner = scanner
+
+        # initialize by getting first symbol from scanner
+        self.current_symbol = scanner.get_symbol()
+
+    def throw_error(self):
+        """Error"""
+        pass
+
+    def match_symbol(self, symbol_id: int):
+        """Checks if current symbol matches expected"""
+        if symbol_id == self.current_symbol.id:
+            self.current_symbol = self.scanner.get_symbol()
+        else:
+            self.throw_error()
+
+        
+
+
+    def parse_devices_statement(self):
+        """Parse definition statement from DEVICES block"""
+        pass
+
+    def parse_connection(self):
+        """Parse connection statement from CONNECTIONS block"""
+
+        pass
+
+    def parse_monitor_statement(self):
+        """Parse monitor statement from MONITORS block"""
+        pass
+
 
     def parse_network(self):
         """Parse the circuit definition file."""
         # For now just return True, so that userint and gui can run in the
         # skeleton code. When complete, should return False when there are
         # errors in the circuit definition file.
+
+
+
+
         return True
